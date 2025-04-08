5a33ced00b39d36a941c9b95ab0f4ba1e951751f
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-04-08 10:09:57 +0200

    its done

--- Speedcode2/Program.cs
+++ Speedcode2/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] Ingredients = { "200 g Hvedemel(ca. 3 1/2 dl)", "2 teskefulde Bagepulver", "1/2 teskefuld Fint salt", "2 Æg", "150 g Sukker (ca. 1 3/4 dl)","1 teskefuld Vaniljesukker", "125 g smeltet smør", "2 Modne Banaer", "100 g mørk chokolade" };
+            List<string> IngredientsList = new List<string>(Ingredients);
+            for (int i = 0; i < IngredientsList.Count; i++)
+            {
+                Console.WriteLine(IngredientsList[i]);
+            }
+            Console.WriteLine("vi skal bage banankage");
+
+
         }
     }
 }
